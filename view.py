9c1175683a8c595755e5d6f72e3e2937df9ccbcd
Tarcísio Frederico <tarcisiosuporte@yahoo.com.br>
2023-09-14 17:16:21 -0300

    Estudando-tupla
    Estudos de tupla e listas

---
+++ view.py
@@ -0,0 +1,27 @@
+# Lista é uma coleção ordenada e mutável. Permite membros ducplicados.
+lista = ["automóvel", True, 2 , 2.5]
+print(lista)
+print(type(lista))
+print("-"*30)
+
+
+#Tupla é uma coleção ordenada e imutavel. Permite membros duplicados.
+tupla = ("avião", True, 2, 2.5)
+print(tupla)
+print(type(tupla))
+print("-"*30)
+
+# O dicionário é uma coleção ordenada e mutavél. nenhum membro duplicado.
+dicionario = {"Suporte":"Tarcísio", "Lógica":False, "numero": 10, "othernumber":10.5}
+print(dicionario)
+print(type(dicionario))
+#ACESSO NO DICIONARIO A CHAVE SUPORTE QUE RETORNA O TARCÍSIO NESSE EXEMPLO.
+print(dicionario["Suporte"])
+print("-"*30)
+
+# o CONJUNTO (SET) é uma coleção não ordenada e não indexada. Nenhum membro duplicado.
+
+Conjunto = {"avião", True, 2,10.65}
+print(Conjunto)
+print(type(Conjunto))
+
